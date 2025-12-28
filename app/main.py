d5969fe5e52f7a26b91e08b5211d2d80a04b49df
Jakub <jakubczupajlo@gmail.com>
2025-12-28 19:23:14 +0100

    Implement full CarWashStation logic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-class Car:
-    # write your code here
-    pass
+return round(total_income, 1)
 
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / 
+                 self.distance_from_city_center)
+        return round(price, 1)
 
-class CarWashStation:
-    # write your code here
-    pass
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, new_rating: int) -> None:
+        total_score = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        self.average_rating = round((total_score + new_rating) / self.count_of_ratings, 1)
