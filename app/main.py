a1aafa22c8f89512cb628d67b3ad45f884a5a493
Jakub <jakubczupajlo@gmail.com>
2025-12-28 19:57:23 +0100

    Apply strict PEP8 formatting

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class CarWashStation:
         distance_from_city_center: float,
         clean_power: int,
         average_rating: float,
-        count_of_ratings: int,
+        count_of_ratings: int
     ):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -22,17 +22,16 @@ class CarWashStation:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
+                # Obliczamy cenę przed zmianą czystości auta
                 total_income += self.calculate_washing_price(car)
+                # Myjemy auto
                 self.wash_single_car(car)
+        
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = (
-            car.comfort_class
-            * (self.clean_power - car.clean_mark)
-            * self.average_rating
-            / self.distance_from_city_center
-        )
+        price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / 
+                 self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -40,9 +39,7 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, new_rating: int) -> None:
+        # Obliczanie nowej średniej: (suma starych ocen + nowa ocena) / nowa liczba ocen
         total_score = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round(
-            (total_score + new_rating) / self.count_of_ratings, 1
-        )
-
+        self.average_rating = round((total_score + new_rating) / self.count_of_ratings, 1)
