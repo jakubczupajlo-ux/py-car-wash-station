a510df25fc075c43797df45ec81e60a8f4125bd5
Jakub <jakubczupajlo@gmail.com>
2026-02-20 18:30:10 +0100

    Washing Station

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+from typing import List
+
+
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -11,20 +19,12 @@ class CarWashStation:
         distance_from_city_center: float,
         clean_power: int,
         average_rating: float,
-        count_of_ratings: int,
-    ):
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-
-    def serve_cars(self, cars: list) -> float:
-        total_income = 0.0
-        for car in cars:
-            if car.clean_mark < self.clean_power:
-                total_income += self.calculate_washing_price(car)
-                self.wash_single_car(car)
-        return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         price = (
@@ -39,9 +39,17 @@ class CarWashStation:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
+    def serve_cars(self, cars: List[Car]) -> float:
+        total_income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(total_income, 1)
+
     def rate_service(self, new_rating: int) -> None:
-        total_score = self.average_rating * self.count_of_ratings
+        total_rating_sum = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         self.average_rating = round(
-            (total_score + new_rating) / self.count_of_ratings, 1
+            (total_rating_sum + new_rating) / self.count_of_ratings, 1
         )
